9d8255cf994e8e1aa9523d94da23409afef3df6e
Alyza Diaz Rodriguez <diazrodrigueza@wit.edu>
2021-06-17 12:56:33 -0400

    Updated popup window with background fading.

--- JAVASCRIPT/home.js
+++ JAVASCRIPT/home.js
@@ -6,9 +6,15 @@ document.addEventListener("DOMContentLoaded", () =>{
 function handleClick(event){
     const pop = document.querySelector(".popup")
     const close_button = document.querySelector(".close")
-    pop.style.display = "block";
+    pop.style.display = "flex";
+
+    const bar = document.getElementById("top-bar");
+    bar.style.backgroundColor = "rgba(0,0,0,0.7)";
+    
 
     close_button.addEventListener("click", ()=>{
         pop.style.display = "none";
+        bar.style.backgroundColor = "white";
+        bar.style.opacity = 1;
     })
 }
