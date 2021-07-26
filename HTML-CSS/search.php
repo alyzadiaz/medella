3c355e14a72f84978733746a24c7bd56f382f771
Alyza Diaz Rodriguez <diazrodrigueza@wit.edu>
2021-07-26 15:23:57 -0400

    Diagnosis detail pages set up

--- HTML-CSS/search.php
+++ HTML-CSS/search.php
@@ -9,7 +9,7 @@
         die("Connection failed: " . $conn->connect_error);
     }
 
-    $sql = "SELECT * FROM Medella.disease ORDER BY name";
+    $sql = "SELECT name,id FROM Medella.disease ORDER BY name";
     $result = mysqli_query($conn, $sql);
 ?>
 
@@ -20,6 +20,8 @@
         <link rel="stylesheet" type="text/css" href="new-home.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="icon" href="../RESOURCES/lavender.svg">
+
+        
 
         <style>
             @import url('https://fonts.googleapis.com/css2?family=News+Cycle&display=swap');
@@ -39,17 +41,24 @@
         <div class="search-body">
             <div class="search-results">
                 <h1>Search Results</h1>
+                
                 <div class="results-box">
                     <?php while ($row = mysqli_fetch_array($result)): ?>
-                    <?php $name = $row['name']?>
+                    <?php 
+                        $name = $row['name'];
+                        $id = $row['id'];
+                    ?>
 
-                    <a href="../HTML-CSS/diagnosis.html" class="result"><?php echo $name; ?></a>
+                    <form class="id-form" action="../HTML-CSS/diag.php" method="post">
+                        <button class="underline-button result"><?php echo $name;?></button>
+                        <input class="hidden" type="number" name="id" value=<?php echo $id?>>
+                    </form>
 
                     <?php endwhile; ?>
-
                 </div>
             </div>
         </div>
+        <script src="../JAVASCRIPT-PHP/search.js"></script>
     </body>
     <footer>
         
